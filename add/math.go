4f6b342f5c6a81c9b2fd9706098f9282982311d5
路飞君1234 <lufeijun_1234@126.com>
2025-12-04 13:59:27 +0800

    泛型加减乘除工具

---
+++ add/math.go
@@ -0,0 +1,29 @@
+package add
+
+// 定义支持的数字类型
+type Number interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+}
+
+// 泛型加法
+func Add[T Number](a, b T) T {
+	return a + b
+}
+
+// 泛型减法
+func Subtract[T Number](a, b T) T {
+	return a - b
+}
+
+// 泛型乘法
+func Multiply[T Number](a, b T) T {
+	return a * b
+}
+
+// 泛型除法
+func Divide[T Number](a, b T) T {
+	if b == 0 {
+		panic("division by zero")
+	}
+	return a / b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lufeijun/go-tools
+
+go 1.25.0
